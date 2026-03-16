a76b5fe22f7279a494479accd5388e2cf1569a24
aliieroglu <alibugraeroglu@gmail.com>
2026-03-16 20:39:21 +0300

    tracking service file structure

---
+++ tracking-service/tests/test_db_adapter.py
@@ -0,0 +1,12 @@
+import pytest
+import asyncio
+
+@pytest.mark.asyncio
+async def test_redis_adapter_connection():
+    # TODO: Implement test for Redis connection
+    pass
+
+@pytest.mark.asyncio
+async def test_redis_adapter_execution():
+    # TODO: Implement test for Redis execution
+    pass
